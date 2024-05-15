83b113d14c8d747f3a03dd5f14977f061dc56b67
Dimitri MINTSA-MI OBAME <135711513+dmtsolution@users.noreply.github.com>
2024-05-15 16:37:26 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,6 +3,6 @@
     <span class="text-muted">&copy; 2024 - One-Phase Simplex / Tous droits réservés</span>
   </div>
   <div class="container text-center">
-    <span class="text-muted">Développé par Ruth ALEMAWO - Sydney RABBY - <a href="https://www.linkedin.com/in/dimitri-mintsa-b11352221/" target="_blank"> Dimitri MINTSA</a></span>
+    <span class="text-muted">Développé par <a href="https://www.linkedin.com/in/abla-ruth-alemawo-847368295/" target="_blank">Ruth ALEMAWO</a> - <a href="https://www.linkedin.com/in/rabby-sydneydeslandn-souassani-9580712a9/" target="_blank">Sydney RABBY</a> - <a href="https://www.linkedin.com/in/dimitri-mintsa-b11352221/" target="_blank">Dimitri MINTSA</a></span>
   </div>
 </footer>
